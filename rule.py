efe79171949a280be4cf3832c0f9a11531e3df8a
sanketb <sanketb@stud.ntnu.no>
2021-03-01 17:33:40 +0100

    rule klasse

--- rule.py
+++ rule.py
@@ -1,5 +1,6 @@
 """This module contains the Rule-class"""
 from typing import Callable
+from keypad import Keypad
 
 
 class Rule:
@@ -10,5 +11,17 @@ class Rule:
     expected_signal: chr or Callable
     action: Callable
 
-    def match(self, state: str or Callable, signal: chr or Callable) -> bool:
-        pass
+
+    def match(self, state: str or Callable, signal: int or Callable):
+        if signal_is_digit(signal):
+            if state == self.source_state and signal == self.expected_signal:
+                return self.action
+            else:
+                state = self.source_state
+
+
+
+def signal_is_digit(signal):
+    return 0 <=ord(signal) <=9
+
+
