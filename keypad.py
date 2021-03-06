6051e8fab2b81d14340bb8c2a39331febb7378dd
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-03-06 12:12:11 +0100

    Finished Keypad and added some documentation

--- keypad.py
+++ keypad.py
@@ -1,29 +1,33 @@
 from GPIOSimulator_v5 import *
-from time import sleep
+from time import sleep, time
 
 GPIO = GPIOSimulator()
-ROW_PINS = [PIN_KEYPAD_ROW_0, PIN_KEYPAD_ROW_1, PIN_KEYPAD_ROW_2, PIN_KEYPAD_ROW_3]
-COL_PINS = [PIN_KEYPAD_COL_0, PIN_KEYPAD_COL_1, PIN_KEYPAD_COL_2]
 
-# Setup
-for row_pin in ROW_PINS:
+# Setup, should be in init maybe?
+for row_pin in keypad_row_pins:
     GPIO.setup(row_pin, GPIO.OUT)
-for col_pin in COL_PINS:
+for col_pin in keypad_col_pins:
     GPIO.setup(col_pin, GPIO.IN, state=GPIO.LOW)
 
 KEYS = [i for i in range(1, 10)]
 KEYS.extend(["*", 0, "#"])
 
+MAX_PRESS_DURATION = 1
 
 class Keypad:
 
+    def __init__(self):
+        self.prev_pressed = None
+        self.t_prev_pressed = 0
+
     def poll(self):
-        for r in range(0, len(ROW_PINS)):
-            row_pin = ROW_PINS[r]
+        """Returns current key pressed, None is nothing is pressed"""
+        for r in range(0, len(keypad_row_pins)):
+            row_pin = keypad_row_pins[r]
             GPIO.output(row_pin, GPIO.HIGH)
 
-            for c in range(0, len(COL_PINS)):
-                col_pin = COL_PINS[c]
+            for c in range(0, len(keypad_col_pins)):
+                col_pin = keypad_col_pins[c]
                 if GPIO.input(col_pin) == GPIO.HIGH:
                     GPIO.output(row_pin, GPIO.LOW)
                     return KEYS[r * 3 + c]
@@ -32,4 +36,18 @@ class Keypad:
         return None
 
     def get_key_pressed(self):
-        pass
+        """Polls until a key is pressed and returns the key. If the same key is pressed longer than
+            MAX_PRESS_DURATION, it is seen as an extra key press."""
+        pressed = None
+        released = False
+        while pressed is None or (self.prev_pressed is not None and (self.prev_pressed == pressed and
+                                  (not released and time() - self.t_prev_pressed < MAX_PRESS_DURATION))):
+            pressed = self.poll()
+            if pressed is None:
+                released = True
+            sleep(0.008)
+
+        self.prev_pressed = pressed
+        self.t_prev_pressed = time()
+        return pressed
+
