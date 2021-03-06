bc635535cd9b250bba52ef4710db38bb3f7d4573
kavusi98 <kavusikan1998@gmail.com>
2021-03-06 21:50:33 +0100

    Started on Main.py (#3)

--- fsm.py
+++ fsm.py
@@ -1,12 +1,21 @@
 from rule import Rule
 from inspect import isfunction
+
+
 class FSM:
 
     def __init__(self):
         self.rules = []
+
     def add_rule(self, rule):
-        if(isinstance(rule, Rule)):
+        if (isinstance(rule, Rule)):
             self.rules.append(rule)
 
     def get_next_signal(self):
+        pass
+
+    def get_start_state(self):
+        pass
+
+    def get_end_state(self):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+from inspect import isfunction
+from rule import Rule
+from fsm import FSM
+from agent import KPCAgent
+from keypad import Keypad
+from ledboard import LEDBoard
+
 def main():
     # Skriv kode her
-    pass
+    rule = Rule() # Skal vel være i FSM?
+    fsm = FSM()
+    agent = KPCAgent()
+    #keypad = Keypad() # i Agent
+    #ledboard = LEDBoard() # i Agent
+    state = fsm.get_start_state()
+
+    while not state == fsm.get_end_state():
+        signal = agent.get_next_signal()
+        for rule in fsm.rules:
+            if rule.match(state, signal):
+                state = rule.state2
+                agent.do_action(rule.action, signal)
+                #go to
+            break
+        break
+
+
+    #agent keypad, led shutdown
+    agent.led_board.turn_off()
 
 
 if __name__ == '__main__':
